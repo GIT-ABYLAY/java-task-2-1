ca922a9a19ac916f289d307365dbbffa6bbecd21
Neto test <d.a.auken-00@mail.ru>
2023-09-03 22:57:34 +0600

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 10000;
+        int milePrice = 20;
+
+        System.out.println("Цена билета:" + ticketPrice);
+        System.out.println("стоимость бонусной мили:" + milePrice);
+        System.out.println("Количество бонусов:" + ticketPrice / milePrice);
+    }
+}
